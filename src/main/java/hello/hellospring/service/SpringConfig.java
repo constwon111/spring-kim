6c7b0ae71206ad3c2380cec064c52649f98444fa
constwon111 <constwon111@gmail.com>
2023-01-17 01:31:09 +0900

    #11 change jdbc template to jpa

--- src/main/java/hello/hellospring/service/SpringConfig.java
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -1,21 +1,27 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
-	private final DataSource dataSource;
+//	private final DataSource dataSource;
+//
+//	public SpringConfig(DataSource dataSource) {
+//		this.dataSource = dataSource;
+//	}
 
-	public SpringConfig(DataSource dataSource) {
-		this.dataSource = dataSource;
+	final private EntityManager em;
+
+	public SpringConfig(EntityManager em) {
+		this.em = em;
 	}
+
 
 	@Bean
 	public MemberService memberService() {
@@ -26,7 +32,8 @@ public class SpringConfig {
 	public MemberRepository memberRepository() {
 //		return new MemoryMemberRepository();
 //		return new JdbcMemberRepository(dataSource);
-		return new JdbcTemplateMemberRepository(dataSource);
+//		return new JdbcTemplateMemberRepository(dataSource);
+		return new JpaMemberRepository(em);
 
 	}
 
