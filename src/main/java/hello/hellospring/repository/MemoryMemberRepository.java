fdc389fb83a05e8caa042ce85dd36cea4efa08a8
constwon111 <constwon111@gmail.com>
2023-01-16 22:26:08 +0900

    #4 register member controller using bean

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,12 +1,15 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-public class MemoryMemberRepository implements MemberRepository{
-	private static Map<Long,Member> store  =  new HashMap<>();
+@Repository
+public class MemoryMemberRepository implements MemberRepository {
+	private static Map<Long, Member> store = new HashMap<>();
 	private static long sequence = 0L;
+
 	@Override
 	public Member save(Member member) {
 		member.setId(++sequence);
@@ -30,7 +33,7 @@ public class MemoryMemberRepository implements MemberRepository{
 	}
 
 
-	public void clearStore(){
+	public void clearStore() {
 		store.clear();
 	}
 }
